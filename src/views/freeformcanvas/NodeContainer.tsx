4e587680d90ec2e15698d636f14e31833b3f7362
Paul Biberstein <biberstein.paul@gmail.com>
2019-12-26 09:16:46 -0500

    Restructured nodeContainer rendering

--- src/views/freeformcanvas/NodeContainer.tsx
+++ src/views/freeformcanvas/NodeContainer.tsx
@@ -26,84 +26,53 @@ export class NodeContainer extends React.Component<IProps> {
 
     render() {
         let store = this.props.store
-        if (store.isTopLevel) {
-            return (
-                <div className="node-container">
-                    {/* Render links between nodes */}
-                    {this.props.store.LinkedPairs.map(pair => {
-                        let [n1x, n1y] = pair[0].CenterCoords
-                        let [n2x, n2y] = pair[1].CenterCoords
-                        // Swap assignments so n1 is always the node farther to the left
-                        if (n2x < n1x) {
-                            [n1x, n1y, n2x, n2y] = [n2x, n2y, n1x, n1y]
-                        }
-                        let dx = n2x - n1x
-                        let dy = n2y - n1y
-                        // Use pythagorean theorem to calculate line length
-                        let length: number = Math.pow(Math.pow(dx, 2) + Math.pow(dy, 2), 1/2)
-                        // Use inverse tangent to calculate rotation
-                        let rotation: number = Math.atan(dy / dx)
-                        return <div className="node-link" style={{ width: length,
-                            transform: ("translate(" + n1x + "px," + n1y + "px) rotate(" + rotation + "rad)")
-                            }}></div>
-                    })}
 
-                    {/* Render nodes */}
-                    {this.props.store.Nodes.map(nodeStore => {
-                        if (nodeStore instanceof StaticTextNodeStore) {
-                            return (<TextNodeView key={nodeStore.Id} store={nodeStore as StaticTextNodeStore} />)
-                        } else if (nodeStore instanceof VideoNodeStore) {
-                            return (<VideoNodeView key={nodeStore.Id} store={nodeStore as VideoNodeStore} />)
-                        } else if (nodeStore instanceof WebsiteNodeStore) {
-                            return (<WebsiteNodeView key={nodeStore.Id} store={nodeStore as WebsiteNodeStore} />)
-                        } else if (nodeStore instanceof ImageNodeStore) {
-                            return (<ImageNodeView key={nodeStore.Id} store={nodeStore as ImageNodeStore} />)
-                        } else if (nodeStore instanceof NodeCollectionStore) {
-                            return (<NodeContainer key={nodeStore.Id} store={nodeStore as NodeCollectionStore} />)
-                        }
-                    })}
-                </div>
-            )
+        let content = <div className="node-container">
+            {/* Render links between nodes */}
+            {this.props.store.LinkedPairs.map(pair => {
+                let [n1x, n1y] = pair[0].CenterCoords
+                let [n2x, n2y] = pair[1].CenterCoords
+                // Swap assignments so n1 is always the node farther to the left
+                if (n2x < n1x) {
+                    [n1x, n1y, n2x, n2y] = [n2x, n2y, n1x, n1y]
+                }
+                let dx = n2x - n1x
+                let dy = n2y - n1y
+                // Use pythagorean theorem to calculate line length
+                let length: number = Math.pow(Math.pow(dx, 2) + Math.pow(dy, 2), 1/2)
+                // Use inverse tangent to calculate rotation
+                let rotation: number = Math.atan(dy / dx)
+                return <div className="node-link" style={{ width: length,
+                    transform: ("translate(" + n1x + "px," + n1y + "px) rotate(" + rotation + "rad)")
+                    }}></div>
+            })}
+
+            {/* Render nodes */}
+            {this.props.store.Nodes.map(nodeStore => {
+                if (nodeStore instanceof StaticTextNodeStore) {
+                    return (<TextNodeView key={nodeStore.Id} store={nodeStore as StaticTextNodeStore} />)
+                } else if (nodeStore instanceof VideoNodeStore) {
+                    return (<VideoNodeView key={nodeStore.Id} store={nodeStore as VideoNodeStore} />)
+                } else if (nodeStore instanceof WebsiteNodeStore) {
+                    return (<WebsiteNodeView key={nodeStore.Id} store={nodeStore as WebsiteNodeStore} />)
+                } else if (nodeStore instanceof ImageNodeStore) {
+                    return (<ImageNodeView key={nodeStore.Id} store={nodeStore as ImageNodeStore} />)
+                } else if (nodeStore instanceof NodeCollectionStore) {
+                    return (<NodeContainer key={nodeStore.Id} store={nodeStore as NodeCollectionStore} />)
+                }
+            })}
+        </div>
+
+        if (store.isTopLevel) {
+            return content
         } else {
+            // If this NodeContainer exists as a nested collection, we need to add some extra HTML
             return (
                 <div className="node" style={{ transform: store.Transform, width: store.Width, height: store.Height }}>
                     <TopBar store={store} />
                     <div className="no-scroll-box node-boundary">
                         <div className="content">
-                            <div className="node-container">
-                                {/* Render links between nodes */}
-                                {this.props.store.LinkedPairs.map(pair => {
-                                    let [n1x, n1y] = pair[0].CenterCoords
-                                    let [n2x, n2y] = pair[1].CenterCoords
-                                    // Swap assignments so n1 is always the node farther to the left
-                                    if (n2x < n1x) {
-                                        [n1x, n1y, n2x, n2y] = [n2x, n2y, n1x, n1y]
-                                    }
-                                    let dx = n2x - n1x
-                                    let dy = n2y - n1y
-                                    // Use pythagorean theorem to calculate line length
-                                    let length: number = Math.pow(Math.pow(dx, 2) + Math.pow(dy, 2), 1/2)
-                                    // Use inverse tangent to calculate rotation
-                                    let rotation: number = Math.atan(dy / dx)
-                                    return <div className="node-link" style={{ width: length,
-                                        transform: ("translate(" + n1x + "px," + n1y + "px) rotate(" + rotation + "rad)")
-                                        }}></div>
-                                })}
-                                {/* Render nodes */}
-                                {this.props.store.Nodes.map(nodeStore => {
-                                    if (nodeStore instanceof StaticTextNodeStore) {
-                                        return (<TextNodeView key={nodeStore.Id} store={nodeStore as StaticTextNodeStore} />)
-                                    } else if (nodeStore instanceof VideoNodeStore) {
-                                        return (<VideoNodeView key={nodeStore.Id} store={nodeStore as VideoNodeStore} />)
-                                    } else if (nodeStore instanceof WebsiteNodeStore) {
-                                        return (<WebsiteNodeView key={nodeStore.Id} store={nodeStore as WebsiteNodeStore} />)
-                                    } else if (nodeStore instanceof ImageNodeStore) {
-                                        return (<ImageNodeView key={nodeStore.Id} store={nodeStore as ImageNodeStore} />)
-                                    } else if (nodeStore instanceof NodeCollectionStore) {
-                                        return (<NodeContainer key={nodeStore.Id} store={nodeStore as NodeCollectionStore} />)
-                                    }
-                                })}
-                            </div>
+                            {content}
                         </div>
                     </div>
                     <ResizeCorner store={store} />
