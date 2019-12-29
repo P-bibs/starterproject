285518500cafc4c644b4737e3d406bde2caa9a58
Paul Biberstein <biberstein.paul@gmail.com>
2019-12-28 20:55:13 -0500

    added text edit node

--- src/stores/TextEditorNodeStore.ts
+++ src/stores/TextEditorNodeStore.ts
@@ -1,4 +1,4 @@
-import { observable, action } from "mobx";
+import { observable, computed, action } from "mobx";
 import { NodeStore } from "./NodeStore";
 
 export class TextEditorNodeStore extends NodeStore {
